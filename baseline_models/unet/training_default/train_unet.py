2f26096d78b7dc2dbcbc8eb283c2da338225c8a8
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2025-11-09 00:05:55 -0800

    another save fix

--- baseline_models/unet/training_default/train_unet.py
+++ baseline_models/unet/training_default/train_unet.py
@@ -606,8 +606,8 @@ def main(cfg: DictConfig) -> float:
                     ckpt_path = os.path.join(save_path_ckpt, f'ckpt_epoch_{epoch+1}_metric_{current_metric:.4f}.mdlus')
                     ckpt_path_res = os.path.join(save_path_ckpt, f'ckpt_epoch_{epoch+1}_metric_{current_metric:.4f}_res.mdlus')
                     if dist.distributed:
-                        model.modules.save(ckpt_path)
-                        res_model.module.save(ckpt_path_res)
+                        model.save(ckpt_path)
+                        res_model.save(ckpt_path_res)
                     else:
                         model.save(ckpt_path)
                         res_model.save(ckpt_path_res)

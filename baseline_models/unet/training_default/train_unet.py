e2ba2af10f5a40b29c17f249804b5376042675e5
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2025-11-18 18:41:51 -0800

    converted model parameter generators to list before creating optimizer

--- baseline_models/unet/training_default/train_unet.py
+++ baseline_models/unet/training_default/train_unet.py
@@ -257,9 +257,9 @@ def main(cfg: DictConfig) -> float:
 
     #joint optimizer
     if cfg.optimizer == 'Adam':
-        joint_optimizer = optim.Adam(model.parameters() + res_model.parameters(), lr=cfg.learning_rate)
+        joint_optimizer = optim.Adam(list(model.parameters()) + list(res_model.parameters()), lr=cfg.learning_rate)
     elif cfg.optimizer == 'AdamW':
-        joint_optimizer = optim.AdamW(model.parameters() + res_model.parameters(), lr=cfg.learning_rate)
+        joint_optimizer = optim.AdamW(list(model.parameters()) + list(res_model.parameters()), lr=cfg.learning_rate)
     else:
         raise ValueError('Optimizer not implemented')
     

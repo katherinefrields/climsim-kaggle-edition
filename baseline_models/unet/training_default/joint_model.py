337496f1091c6ca011eb489bbe21533fa922d78c
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2025-11-20 13:47:49 -0800

    minor bug fix

--- baseline_models/unet/training_default/joint_model.py
+++ baseline_models/unet/training_default/joint_model.py
@@ -99,6 +99,7 @@ class JointModel(nn.Module):
             res_loss, all_params, retain_graph=False, allow_unused=True
         )
         #res_grad = data_utils.joint_get_gradient_vector(self.model_a, self.model_b, none_grad_mode="zero")
+        grads = [grads_det, grads_res]
         
         g_config=ConFIG_update(grads) # calculate the conflict-free direction
         joint_optimizer.zero_grad()

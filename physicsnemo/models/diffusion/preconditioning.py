057ff2056bf5b701f2c080325fcb44d7c4aa2500
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2025-11-25 22:47:23 -0800

    temporarily hardcoded input_padding

--- physicsnemo/models/diffusion/preconditioning.py
+++ physicsnemo/models/diffusion/preconditioning.py
@@ -166,15 +166,16 @@ class EDMPrecond(Module):
         #levels are without padding
         #currently x(batch, target_profile_num*levels+target_scalar_num)
         sigma_data = self.sigma_data.unsqueeze(0)
-        profile_index = self.input_profile_num*(self.vertical_level_num + self.input_padding)
+        profile_index = self.input_profile_num*(self.vertical_level_num + 4)
+        
         sigma_data_profile = sigma_data[:,:profile_index]
         sigma_scalar = sigma_data[:,profile_index:]
         
         # reshape x_profile to (batch, input_profile_num, levels)
-        sigma_data_profile = sigma_data_profile.reshape(-1, self.input_profile_num, self.vertical_level_num + self.input_padding)
+        sigma_data_profile = sigma_data_profile.reshape(-1, self.input_profile_num, self.vertical_level_num + 4)
         
         # broadcast x_scalar to (batch, input_scalar_num, levels)
-        sigma_scalar = sigma_scalar.unsqueeze(2).expand(-1, -1, self.vertical_level_num + self.input_padding)
+        sigma_scalar = sigma_scalar.unsqueeze(2).expand(-1, -1, self.vertical_level_num + 4)
         
         #concatenate x_profile, x_scalar, x_loc to (batch, input_profile_num+input_scalar_num, levels)
         sigma_data = torch.cat((sigma_data_profile, sigma_scalar), dim=1)

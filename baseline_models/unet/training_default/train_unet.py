63eca63f7deb5c26e4ad490b707e0180629d633a
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-02-16 13:42:04 -0800

    refactored conditioning to properly calculate  in condition channel number

--- baseline_models/unet/training_default/train_unet.py
+++ baseline_models/unet/training_default/train_unet.py
@@ -220,10 +220,14 @@ def main(cfg: DictConfig) -> float:
     res_channel_mult = OmegaConf.to_container(cfg.diffusion_model.channel_mult, resolve = True)
     #res_resample_filter = OmegaConf.to_container(cfg.diffusion_model.resample_filter, resolve = True)
     
-    if cfg.diffusion_model.condition_type == 'input_output':
-        cond_channels = (data.target_profile_num  + data.target_scalar_num + data.input_profile_num + data.input_scalar_num)*64
-    else:
-        cond_channels = 8192
+    if cfg.diffusion_model.condition_locaiton == 'embedding':
+        if cfg.diffusion_model.condition_type == 'input_output':
+            cond_channels = (data.target_profile_num  + data.target_scalar_num + data.input_profile_num + data.input_scalar_num)*64
+        else:
+            cond_channels = 8192
+    elif cfg.diffusion_model.condition_location == 'middle':
+        if cfg.diffusion_model.condition_type == 'input_output':
+            cond_channels = data.target_profile_num  + data.target_scalar_num + data.input_profile_num + data.input_scalar_num
     
     res_model = EDMPrecond(
         img_resolution=cfg.diffusion_model.seq_resolution,         # vertical levels

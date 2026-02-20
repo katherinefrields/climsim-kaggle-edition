c0df1c8862dbd0bc51fcbd30a9606e4f1ffc3ff8
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-02-20 14:55:33 -0800

    fix minor bug

--- physicsnemo/models/diffusion/dhariwal_unet.py
+++ physicsnemo/models/diffusion/dhariwal_unet.py
@@ -377,7 +377,7 @@ class DhariwalUNet(modulus.Module):
         
         
         if self.condition_location == 'front':
-            x.cat([x, cond], dim=1)
+            torch.cat([x, cond], dim=1)
             
         cond_pyr = {}
         if cond is not None and self.condition_location == 'cross':

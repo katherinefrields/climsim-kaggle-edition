816f3cb90b5521ee06374cd6e24db41d2f4f5307
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-03-06 14:39:20 -0800

    added case directory location

--- online_ensembling/v2_rh_mc/diff_test_run.py
+++ online_ensembling/v2_rh_mc/diff_test_run.py
@@ -20,7 +20,7 @@ case_prefix = 'test_case_4'
 #case_dir = '/scratch/'
 top_dir  = os.getenv('HOME')
 scratch_dir = os.getenv('SCRATCH')
-#case_dir = f'{scratch_dir}/hugging/E3SM-MMF-online-runs'
+case_dir = f'{scratch_dir}/hugging/E3SM-MMF-online-runs'
 src_dir  = top_dir+'/E3SM/' # branch => whannah/mmf/ml-training
 #user_cpp = '-DMMF_ML_TRAINING' # for saving ML variables
 # user_cpp = '-DMMF_NN_EMULATOR -DMMF_NN_EMULATOR_DIAG_PARTIAL -DMMF_NN_EMULATORDEBUG -DTORCH_MMF_NN_EMULATOR_TEST' # NN hybrid test

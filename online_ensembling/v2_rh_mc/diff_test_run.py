a651e8782bdd05a149da9d1176c981c0c210d47e
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-03-06 21:51:54 -0800

    set continue run to false for new runs

--- online_ensembling/v2_rh_mc/diff_test_run.py
+++ online_ensembling/v2_rh_mc/diff_test_run.py
@@ -13,7 +13,7 @@ import shutil, glob
 #acct = 'm4334'
 acct = os.environ.get("MMF_NN_SLURM_ACCOUNT", "m4334")
 
-case_prefix = 'test_case_16'
+case_prefix = 'test_case_17'
 # exe_refcase = 'ftorch_test'
 # Added extra physics_state and cam_out variables.
 
@@ -45,7 +45,7 @@ newcase      = True
 config       = True
 build        = True
 submit       = True
-continue_run = True #what does this do? - Katherine Frields
+continue_run = False #what does this do? - Katherine Frields
 src_mod_atm  = False 
 
 debug_mode = False

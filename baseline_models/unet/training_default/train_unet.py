7f23e299be302beda94a655b1f9593186ee33772
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-03-07 15:41:57 -0800

    fix debug line

--- baseline_models/unet/training_default/train_unet.py
+++ baseline_models/unet/training_default/train_unet.py
@@ -872,7 +872,7 @@ def main(cfg: DictConfig) -> float:
                                             qn_lbd = torch.tensor(qn_lbd, dtype=torch.float32).to(device)).to(device)
                 save_path_wrapped = os.path.join(wrapped_directory, filename.replace('.mdlus', '_wrapped.pt'))
                 
-                for name, module in model.named_modules():
+                for name, module in joint_inf.named_modules():
                     try:
                         torch.jit.script(module)
                     except Exception as e:

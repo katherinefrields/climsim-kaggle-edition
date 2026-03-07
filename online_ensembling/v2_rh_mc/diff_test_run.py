d5c7b188a4e316aed34f982e0b270d917daee2c7
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-03-06 22:04:45 -0800

    prep run

--- online_ensembling/v2_rh_mc/diff_test_run.py
+++ online_ensembling/v2_rh_mc/diff_test_run.py
@@ -13,7 +13,7 @@ import shutil, glob
 #acct = 'm4334'
 acct = os.environ.get("MMF_NN_SLURM_ACCOUNT", "m4334")
 
-case_prefix = 'test_case_17'
+case_prefix = 'test_case_18'
 # exe_refcase = 'ftorch_test'
 # Added extra physics_state and cam_out variables.
 

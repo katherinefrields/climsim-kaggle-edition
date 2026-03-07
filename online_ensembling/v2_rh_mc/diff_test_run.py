5b626b1e22f44b6db051fe5356e31797ca2bab8b
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-03-06 19:52:11 -0800

    minor syntax error in path

--- online_ensembling/v2_rh_mc/diff_test_run.py
+++ online_ensembling/v2_rh_mc/diff_test_run.py
@@ -24,7 +24,7 @@ scratch_dir = '/scratch'
 case_dir = f'{scratch_dir}/'
 #src_dir  = top_dir+'/E3SM/' # branch => whannah/mmf/ml-training
 #src_dir  = ''+'/nvidia_codes/E3SM_nvlab/' # branch => whannah/mmf/ml-training
-src_dir = 'E3SM'
+src_dir = '/E3SM'
 
 #user_cpp = '-DMMF_ML_TRAINING' # for saving ML variables
 # user_cpp = '-DMMF_NN_EMULATOR -DMMF_NN_EMULATOR_DIAG_PARTIAL -DMMF_NN_EMULATORDEBUG -DTORCH_MMF_NN_EMULATOR_TEST' # NN hybrid test

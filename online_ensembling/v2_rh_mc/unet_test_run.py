95484b88ec613199938caf6ec6d085a713d8e9c3
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-03-17 23:09:38 -0700

    switched over to correct online script for unet run

--- online_ensembling/v2_rh_mc/unet_test_run.py
+++ online_ensembling/v2_rh_mc/unet_test_run.py
@@ -8,33 +8,42 @@ def run_cmd(cmd): print('\n'+clr.GREEN+cmd+clr.END) ; os.system(cmd); return
 #---------------------------------------------------------------------------------------------------
 import os, datetime, subprocess as sp, numpy as np
 import shutil, glob
+
+# Enable Python proxy for MMF NN emulator
+#os.environ["MMF_USE_PYTORCH_FORTRAN_PROXY"] = "1"
 #newcase,config,build,clean,submit,continue_run = False,False,False,False,False,False
 
-acct = 'm4334'
+#acct = 'm4334'
+acct = os.environ.get("MMF_NN_SLURM_ACCOUNT", "m4334")
 
-case_prefix = 'unet_2_day_output'
+case_prefix = '3_day_unet_test'
 # exe_refcase = 'ftorch_test'
 # Added extra physics_state and cam_out variables.
 
-top_dir  = os.getenv('HOME')
-scratch_dir = os.getenv('SCRATCH')
-case_dir = f'{scratch_dir}/hugging/E3SM-MMF-online-runs'
-src_dir  = top_dir+'/nvidia_codes/E3SM_nvlab/' # branch => whannah/mmf/ml-training
+top_dir  = "/climsim"
+scratch_dir = '/scratch'
+#top_dir  = os.getenv('HOME')
+#scratch_dir = os.getenv('SCRATCH')
+case_dir = f'{scratch_dir}/'
+src_dir  = top_dir+'/E3SM/' # branch => whannah/mmf/ml-training
+#src_dir  = ''+'/nvidia_codes/E3SM_nvlab/' # branch => whannah/mmf/ml-training
+#src_dir = '/E3SM'
+
 #user_cpp = '-DMMF_ML_TRAINING' # for saving ML variables
 # user_cpp = '-DMMF_NN_EMULATOR -DMMF_NN_EMULATOR_DIAG_PARTIAL -DMMF_NN_EMULATORDEBUG -DTORCH_MMF_NN_EMULATOR_TEST' # NN hybrid test
 user_cpp = '-DMMF_NN_EMULATOR' # NN hybrid test
 # # src_mod_atm_dir = '/global/homes/s/sungduk/repositories/ClimSim-E3SM-Hybrid/'
 # # old ftorch path below. For some reason, the install folder is located in FTorch not src.
 # ftorch_path = '/global/cfs/cdirs/m4334/shared/FTorch/src/install'
-ftorch_path = '/global/cfs/cdirs/m4334/shared/FTorch/install'
-os.environ["FTorch_ROOT"] = ftorch_path
-os.environ["NVCC_WRAPPER_DEFAULT_OPTIONS"] = "-arch=sm_80"
-os.environ["KOKKOS_NVCC_FLAGS"] = "-arch=sm_80"
+
+pytorch_fortran_path = '/opt/pytorch-fortran'
+os.environ["pytorch_proxy_ROOT"] = pytorch_fortran_path
+os.environ["pytorch_fort_proxy_ROOT"] = pytorch_fortran_path
 # RESTART
 runtype = 'branch' # startup, hybrid,  branch
 refdate = '0002-12-30' # only works for branch (and hybrid?)
 reftod = '00000' # or 21600, 43200, 64800
-clean        = True
+clean        = False
 newcase      = True
 config       = True
 build        = True
@@ -46,11 +55,11 @@ debug_mode = False
 
 dtime = 1200 # set to 0 to use a default value 
 
-#stop_opt,stop_n,resub,walltime = 'nmonths',1, 1, '00:30:00'
+#stop_opt,stop_n,resub,walltime = 'nmonths',3, 0, '05:00:00'
 #stop_opt,stop_n,resub,walltime = 'nmonths',13, 0,'00:10:00'
 stop_opt,stop_n,resub,walltime = 'ndays',3, 0,'00:30:00'
 
-ne,npg=4,2;  num_nodes=2  ; grid=f'ne{ne}pg{npg}_ne{ne}pg{npg}'
+ne,npg=4,2;  num_nodes=1  ; grid=f'ne{ne}pg{npg}_ne{ne}pg{npg}'
 # ne,npg=30,2; num_nodes=32 ; grid=f'ne{ne}pg{npg}_ne{ne}pg{npg}'
 # ne,npg=30,2; num_nodes=32 ; grid=f'ne{ne}pg{npg}_oECv3' # bi-grid for AMIP or coupled
 
@@ -61,7 +70,7 @@ compset,arch   = 'F2010-MMF1','GNUCPU'
 # (MMF1: Note that MMF_VT is tunred off for MMF_NN_EMULATOR in $E3SMROOT/components/eam/cime_config/config_component.xml)  
 
 # queue = 'regular'
-queue = 'debug'
+queue = 'regular'
 
 # case_list = [case_prefix,arch,compset,grid]
 case_list = [case_prefix, ]
@@ -71,24 +80,34 @@ if debug_mode: case_list.append('debug')
 case='.'.join(case_list)
 #---------------------------------------f------------------------------------------------------------
 # MMF_NN_EMULATOR
-f_torch_model = '/pscratch/sd/k/kfrields/hugging/E3SM-MMF_saved_models/diffusion_models/model_wrapping_test/wrapped_unet_model.pt'
+torch_model = '/storage/shared_e3sm/saved_models/wrapper/wrapped_unet_model.pt'
 
+#torch_model = '/scratch/sd/k/kfrields/hugging/E3SM-MMF_saved_models/diffusion_models/wrap_testing/wrapped_unet_model.pt'
+#torch_model = '/storage/hugging/E3SM-MMF_saved_models/diffusion_models/wrap_testing/wrapped_unet_model.pt'
+#/storage/shared_e3sm/saved_models/wrapper
+inputlength = 557
+outputlength = 368
+cb_nn_var_combo = 'v2'
+input_rh = '.true.'
 cb_spinup_step = 5
-f_cb_strato_water_constraint = '.true.'
-f_cb_do_ramp = '.false.'
-f_cb_ramp_option = 'step'
+cb_strato_water_constraint = '.true.' # set .true. to use stratospheric water constraint to remove all stratospheric clouds and set dqv/dt in strato to 0
+cb_partial_coupling = '.false.'
+cb_do_ramp = '.false.'
+cb_ramp_option = 'step'
 cb_ramp_factor = 1.0
 cb_ramp_step_0steps = 80
 cb_ramp_step_1steps = 10
-cb_use_cuda = '.true.'
 
-
+# check if MMF_ML_TRAINING is in user_cpp, then either no -DMMF_NN_EMULATOR or f_cb_partial_coupling need to be true, otherwise raise error
+if 'MMF_ML_TRAINING' in user_cpp:
+   if 'MMF_NN_EMULATOR' in user_cpp:
+      if cb_partial_coupling == '.false.':
+         raise ValueError('If MMF_NN_EMULATOR is used with MMF_ML_TRAINING, cb_partial_coupling must be true.')
 #---------------------------------------------------------------------------------------------------
 print('\n  case : '+case+'\n')
 
-if 'CPU' in arch : max_mpi_per_node,atm_nthrds  = 64,1 ; max_task_per_node = 64
-if 'GPU' in arch : max_mpi_per_node,atm_nthrds  =  4,8 ; max_task_per_node = 32
-if arch=='CORI'  : max_mpi_per_node,atm_nthrds  = 64,1
+if 'CPU' in arch : max_mpi_per_node,atm_nthrds  =  8,1 ; max_task_per_node = 8
+if 'GPU' in arch : max_mpi_per_node,atm_nthrds  =  2,8 ; max_task_per_node = 16
 atm_ntasks = max_mpi_per_node*num_nodes
 #---------------------------------------------------------------------------------------------------
 if newcase :
@@ -96,17 +115,17 @@ if newcase :
    case_scripts_dir=f'{case_dir}/{case}' 
    if os.path.isdir(f'{case_dir}/{case}'): exit('\n'+clr.RED+f'This case already exists: \n{case_dir}/{case}'+clr.END+'\n')
    cmd = f'{src_dir}/cime/scripts/create_newcase -case {case} --script-root {case_scripts_dir} -compset {compset} -res {grid}  '
-   if arch=='GNUCPU' : cmd += f' -mach pm-cpu -compiler gnu    -pecount {atm_ntasks}x{atm_nthrds} '
-   if arch=='GNUGPU' : cmd += f' -mach pm-gpu -compiler gnugpu -pecount {atm_ntasks}x{atm_nthrds} '
-   if arch=='CORI'   : cmd += f' -mach cori-knl -pecount {atm_ntasks}x{atm_nthrds} '
+   # changed from docker-climsim to pm-cpu
+   if arch=='GNUCPU' : cmd += f' -mach docker-climsim -compiler gnu    -pecount {atm_ntasks}x{atm_nthrds} '
+   if arch=='GNUGPU' : cmd += f' -mach docker-climsim -compiler gnugpu -pecount {atm_ntasks}x{atm_nthrds} '
    run_cmd(cmd)
-   #os.chdir(f'{case_scripts_dir}')
+os.chdir(f'{case_scripts_dir}')
 if newcase :
    case_build_dir=f'{case_dir}/{case}/build'
    case_run_dir=f'{case_dir}/{case}/run'
    short_term_archive_root_dir=f'{case_dir}/{case}/archive'
-   os.makedirs(case_build_dir, exist_ok=True)
-   os.makedirs(case_run_dir, exist_ok=True)
+   os.makedirs(case_build_dir, exist_ok=True)    
+   os.makedirs(case_run_dir, exist_ok=True)    
    os.makedirs(short_term_archive_root_dir, exist_ok=True)    
    run_cmd(f'./xmlchange EXEROOT={case_build_dir}')
    run_cmd(f'./xmlchange RUNDIR={case_run_dir}')
@@ -121,7 +140,7 @@ if newcase :
    # setup branch/hybrid
    if runtype == 'branch':
       run_cmd(f'./xmlchange --file env_run.xml --id RUN_TYPE   --val {runtype}') # 'branch' won't allow change model time steps
-      run_cmd(f'./xmlchange --file env_run.xml --id RUN_REFDIR  --val /global/cfs/cdirs/m4334/shared/shared_e3sm/restart_files/{refdate}-{reftod}')
+      run_cmd(f'./xmlchange --file env_run.xml --id RUN_REFDIR  --val /storage/shared_e3sm/restart_files/{refdate}-{reftod}')
       run_cmd(f'./xmlchange --file env_run.xml --id GET_REFCASE --val TRUE')
       run_cmd(f'./xmlchange --file env_run.xml --id RUN_REFCASE --val E3SM_ML_ne4_rerun.F2010-MMF1')
       run_cmd(f'./xmlchange --file env_run.xml --id RUN_REFDATE --val {refdate}')
@@ -144,32 +163,33 @@ do_aerosol_rad = .false.
 /
 
 &mmf_nn_emulator_nl
-inputlength     = 557
-outputlength    = 368
-cb_nn_var_combo = 'v2'
-input_rh        = .true.
-cb_torch_model  = '{f_torch_model}'
+inputlength     = {inputlength}
+outputlength    = {outputlength}
+cb_nn_var_combo = '{cb_nn_var_combo}'
+input_rh        = {input_rh}
+cb_torch_model  = '{torch_model}'
 cb_spinup_step = {cb_spinup_step}
-cb_partial_coupling = .false.
+cb_partial_coupling = {cb_partial_coupling}
 cb_partial_coupling_vars = 'ptend_t', 'ptend_q0001','ptend_q0002','ptend_q0003', 'ptend_u', 'ptend_v', 'cam_out_PRECC', 'cam_out_PRECSC', 'cam_out_NETSW', 'cam_out_FLWDS', 'cam_out_SOLS', 'cam_out_SOLL', 'cam_out_SOLSD', 'cam_out_SOLLD' 
-cb_do_ramp = {f_cb_do_ramp}
-cb_ramp_option = '{f_cb_ramp_option}'
+cb_do_ramp = {cb_do_ramp}
+cb_ramp_option = '{cb_ramp_option}'
 cb_ramp_factor = {cb_ramp_factor}
 cb_ramp_step_0steps = {cb_ramp_step_0steps}
 cb_ramp_step_1steps = {cb_ramp_step_1steps}
-cb_strato_water_constraint = {f_cb_strato_water_constraint}
-cb_use_cuda = {cb_use_cuda}
+cb_strato_water_constraint = {cb_strato_water_constraint}
 /
 
 &cam_history_nl
-fincl1 = 'PRECT', 'PRECC', 'FLUT', 'CLOUD', 'CLDTOT', 'CLDLOW', 'CLDMED', 'CLDHGH', 'LWCF', 'SWCF', 'LHFLX', 'SHFLX', 'TMQ', 'U850', 'T850', 'Z850', 'U500', 'T500', 'Z500', 'T', 'Q', 'U', 'V', 'PS', 'CLDICE', 'CLDLIQ', 'DTPHYS', 'DQ1PHYS', 'DQ2PHYS', 'DQ3PHYS', 'DUPHYS'
-fincl2 = 'PRECT', 'PRECC', 'FLUT', 'CLOUD', 'CLDTOT', 'CLDLOW', 'CLDMED', 'CLDHGH', 'LWCF', 'SWCF', 'LHFLX', 'SHFLX', 'TMQ', 'U850', 'T850', 'Z850', 'U500', 'T500', 'Z500', 'T', 'Q', 'U', 'V', 'PS', 'CLDICE', 'CLDLIQ', 'DTPHYS', 'DQ1PHYS', 'DQ2PHYS', 'DQ3PHYS', 'DUPHYS'
+fincl1 = 'CLDICE', 'CLDLIQ', 'DTPHYS', 'DQ1PHYS', 'DQ2PHYS', 'DQ3PHYS', 'DUPHYS'
+fincl2 = 'PRECT', 'PRECC', 'FLUT', 'CLOUD', 'CLDTOT', 'CLDLOW', 'CLDMED', 'CLDHGH', 'LWCF', 'SWCF', 'LHFLX', 'SHFLX', 'TMQ', 'U850', 'T850', 'Z850', 'U500', 'T500', 'Z500', 'T', 'Q', 'U', 'V', 'CLDICE', 'CLDLIQ', 'DTPHYS', 'DQ1PHYS', 'DQ2PHYS', 'DQ3PHYS', 'DUPHYS'
 avgflag_pertape = 'A','A'
 nhtfrq = 0,-24
 mfilt  = 0,1
 /
 
                      ''')
+       
+#e3sm.log.260310-061909
 #---------------------------------------------------------------------------------------------------
 # Copy source code modification
 if src_mod_atm :
@@ -184,7 +204,6 @@ if config :
    cpp_defs += ' '+user_cpp+' '
    if cpp_defs != '':
       run_cmd(f'./xmlchange --append --id CAM_CONFIG_OPTS --val \" -cppdefs \' {cpp_defs} \'  \"')
-   # for ClimSim's modified namelist_definition.xml
    if src_mod_atm :
       run_cmd(f'./xmlchange --append --id CAM_CONFIG_OPTS --val \" -usr_src {dir_src_mod} \"')
    run_cmd('./xmlchange PIO_NETCDF_FORMAT=\"64bit_data\" ')
@@ -196,21 +215,23 @@ if build :
    if clean : run_cmd('./case.build --clean')
    run_cmd('./case.build')
 
-# run_cmd(f'cp /pscratch/sd/z/zeyuanhu/e3sm_mlt_scratch/{exe_refcase}/build/e3sm.exe ./build/')
+# run_cmd(f'cp {case_dir}{exe_refcase}/build/e3sm.exe ./build/')
 # run_cmd('./xmlchange BUILD_COMPLETE=TRUE')
 #---------------------------------------------------------------------------------------------------
-if submit :
+if submit : 
    if 'queue' in locals(): run_cmd(f'./xmlchange JOB_QUEUE={queue}')
    run_cmd(f'./xmlchange STOP_OPTION={stop_opt},STOP_N={stop_n},RESUBMIT={resub}')
    run_cmd(f'./xmlchange JOB_WALLCLOCK_TIME={walltime}')
    run_cmd(f'./xmlchange CHARGE_ACCOUNT={acct},PROJECT={acct}')
    continue_flag = 'TRUE' if continue_run else 'False'
-   run_cmd(f'./xmlchange --file env_run.xml CONTINUE_RUN={continue_flag} ')
+   run_cmd(f'./xmlchange --file env_run.xml CONTINUE_RUN={continue_flag} ')   
    #-------------------------------------------------------
-   # run_cmd('./case.submit')
+   #shutil.copy(f"{pytorch_fortran_path}/pytorch_proxy.py", f"{case_run_dir}/pytorch_proxy.py")
+   run_cmd('./case.submit')
 #---------------------------------------------------------------------------------------------------
 # Print the case name again
-print(f'\n  case : {case}\n')
+print(f'\n  case : {case}\n') 
 #---------------------------------------------------------------------------------------------------
 #run_cmd(f'cp -v {__file__} {case_scripts_dir}/launch_script.py')
 #---------------------------------------------------------------------------------------------------
+

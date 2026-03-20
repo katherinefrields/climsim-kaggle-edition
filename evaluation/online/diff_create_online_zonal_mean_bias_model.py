ddf69cf15f97075699468a5e51a81b7f00ec3052
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-03-20 13:47:56 -0700

    fix bug with ignoring stratosphere while scaling

--- evaluation/online/diff_create_online_zonal_mean_bias_model.py
+++ evaluation/online/diff_create_online_zonal_mean_bias_model.py
@@ -399,7 +399,7 @@ def plot_r2_comparison(ds_mmf, ds_nn, num_days, vars_to_plot=None, show=True, sa
 
         # --- right: |joint predictions| / |unet predictions| ---
         ax1 = axs[row_idx, 1]
-        troposphere_vals = pred_ratio_da.values[12:, :]  # exclude stratosphere (levels 0-11)
+        troposphere_vals = pred_ratio_da.values[:, 12:]  # exclude stratosphere (levels 0-11)
         vmin1 = np.nanpercentile(troposphere_vals, 5)
         vmax1 = np.nanpercentile(troposphere_vals, 95)
         im1 = pred_ratio_da.plot(ax=ax1, add_colorbar=False, cmap='RdBu', vmin=vmin1, vmax=vmax1)

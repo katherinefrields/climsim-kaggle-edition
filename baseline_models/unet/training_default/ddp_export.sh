a25134050a63f5847c83f26566300dd35386a8c0
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-04-25 20:49:00 -0700

    double the batch size to reduce overhead delay

--- baseline_models/unet/training_default/ddp_export.sh
+++ baseline_models/unet/training_default/ddp_export.sh
@@ -2,4 +2,4 @@ export RANK=$SLURM_PROCID
 export LOCAL_RANK=$SLURM_LOCALID
 export WORLD_SIZE=$SLURM_NTASKS
 export MASTER_PORT=29500 # default from torch launcher
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:/usr/local/cuda/lib:$LD_LIBRARY_PATH
+#export LD_LIBRARY_PATH=/usr/local/cuda/lib64/stubs:/usr/local/cuda/lib64:/usr/local/cuda/lib:$LD_LIBRARY_PATH

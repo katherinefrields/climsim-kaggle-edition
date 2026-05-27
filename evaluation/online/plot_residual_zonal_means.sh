bedd27698b0a0e1922af1cf3698ab3725572352c
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-05-26 20:56:22 -0700

    prep run

--- evaluation/online/plot_residual_zonal_means.sh
+++ evaluation/online/plot_residual_zonal_means.sh
@@ -2,7 +2,7 @@
 #SBATCH -A m4334
 #SBATCH -C gpu
 #SBATCH -q debug
-#SBATCH -t 00:05:00
+#SBATCH -t 00:30:00
 #SBATCH --ntasks-per-node 1
 #SBATCH --cpus-per-task 32
 #SBATCH --gpus-per-node 1
@@ -29,7 +29,7 @@ DIFF_CONFIG_PATH=${DIFF_CONFIG_PATH:-"${DIFF_BASE}/saved_config.yaml"}
 DIFF_CHECKPOINT_PATH=${DIFF_CHECKPOINT_PATH:-""}
 DIFF_INPUT_NPY=${DIFF_INPUT_NPY:-"/pscratch/sd/j/jerrylin/hugging/E3SM-MMF_ne4/preprocessing/v2_rh_mc/val_set/val_input.npy"}
 DIFF_TARGET_NPY=${DIFF_TARGET_NPY:-"/pscratch/sd/j/jerrylin/hugging/E3SM-MMF_ne4/preprocessing/v2_rh_mc/val_set/val_target.npy"}
-DIFF_N_BATCHES=${DIFF_N_BATCHES:-100}
+DIFF_N_BATCHES=${DIFF_N_BATCHES:-1000}
 DIFF_START_DOY=${DIFF_START_DOY:-""}
 
 echo "Files in precomputed_preds dir:"

bca4350bd14e7b3d1446ee253f87d21d9bcc64b0
Katherine Frields <119017028+katherinefrields@users.noreply.github.com>
2026-05-30 22:49:22 -0700

    prep run

--- evaluation/online/plot_residual_zonal_means.sh
+++ evaluation/online/plot_residual_zonal_means.sh
@@ -20,7 +20,7 @@ REPO_ROOT=/global/u2/k/kfrields/climsim-kaggle-edition
 EXTRA_PKGS=/pscratch/sd/k/kfrields/shifter_packages
 export PYTHONPATH=${EXTRA_PKGS}:${REPO_ROOT}:${PYTHONPATH}
 
-N_BATCHES=${N_BATCHES:-1000} #26280 = 1 year
+N_BATCHES=${N_BATCHES:-100} #26280 = 1 year
 
 # --- Diffusion model ---
 DIFF_BASE=/pscratch/sd/k/kfrields/hugging/E3SM-MMF_saved_models/diffusion_models/24_hour_mse_run_continued/ #/pscratch/sd/k/kfrields/hugging/E3SM-MMF_saved_models/diffusion_models/improved_mse_1_epoch #
@@ -29,7 +29,7 @@ DIFF_CONFIG_PATH=${DIFF_CONFIG_PATH:-"${DIFF_BASE}/saved_config.yaml"}
 DIFF_CHECKPOINT_PATH=${DIFF_CHECKPOINT_PATH:-""}
 DIFF_INPUT_NPY=${DIFF_INPUT_NPY:-"/pscratch/sd/j/jerrylin/hugging/E3SM-MMF_ne4/preprocessing/v2_rh_mc/val_set/val_input.npy"}
 DIFF_TARGET_NPY=${DIFF_TARGET_NPY:-"/pscratch/sd/j/jerrylin/hugging/E3SM-MMF_ne4/preprocessing/v2_rh_mc/val_set/val_target.npy"}
-DIFF_N_BATCHES=${DIFF_N_BATCHES:-1000}
+DIFF_N_BATCHES=${DIFF_N_BATCHES:-100}
 DIFF_START_DOY=${DIFF_START_DOY:-""}
 
 echo "Files in precomputed_preds dir:"
